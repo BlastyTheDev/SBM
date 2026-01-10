b2b500db0a98aab1a248fd4457c30e14c2952d26
Blasty <rmzha2234@gmail.com>
2026-01-10 15:48:16 +1100

    Initial commit

---
+++ src/client/java/dev/blasty/sbm/client/SbmClient.java
@@ -0,0 +1,10 @@
+package dev.blasty.sbm.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SbmClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/dev/blasty/sbm/Sbm.java
@@ -0,0 +1,10 @@
+package dev.blasty.sbm;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Sbm implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
