01e42a74456dab35aab8f3cd8e68a1f134c999a4
Blasty <rmzha2234@gmail.com>
2026-01-10 17:59:19 +1100

    Add pausing functionality to Macro, pause state to be manually checked

--- src/client/java/dev/blasty/sbm/client/macro/Macro.java
+++ src/client/java/dev/blasty/sbm/client/macro/Macro.java
@@ -2,9 +2,15 @@ package dev.blasty.sbm.client.macro;
 
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 public abstract class Macro implements Runnable {
     public static final BlockingQueue<Boolean> tickDelayQueue = new ArrayBlockingQueue<>(1);
+
+    private final ReentrantLock lock = new ReentrantLock();
+    private final Condition unpaused = lock.newCondition();
+    private volatile boolean paused;
 
     protected final void sleep(int ticks) {
         ticks += tickDelayQueue.size();
@@ -15,5 +21,43 @@ public abstract class Macro implements Runnable {
                 throw new RuntimeException(e);
             }
         }
+    }
+
+    protected final void checkPaused() {
+        lock.lock();
+        try {
+            while (paused) {
+                try {
+                    unpaused.await();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public final void pause() {
+        lock.lock();
+        try {
+            paused = true;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public final void resume() {
+        lock.lock();
+        try {
+            paused = false;
+            unpaused.signalAll();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public boolean isPaused() {
+        return paused;
     }
 }
