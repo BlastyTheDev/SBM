5538152120dd5a2dfb880cda17edb3c6eebb0174
Blasty <rmzha2234@gmail.com>
2026-01-10 16:50:41 +1100

    Add Toggle Farming keybind

--- src/client/java/dev/blasty/sbm/client/SbmClient.java
+++ src/client/java/dev/blasty/sbm/client/SbmClient.java
@@ -1,10 +1,17 @@
 package dev.blasty.sbm.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.util.Identifier;
 
 public class SbmClient implements ClientModInitializer {
+    private final KeyBinding.Category keybindCategory = KeyBinding.Category.create(Identifier.of("sbm", "keybinds"));
+    private final KeyBinding toggleFarmingKey = new KeyBinding("Toggle Farming", InputUtil.Type.KEYSYM, InputUtil.GLFW_KEY_F24, keybindCategory);
 
     @Override
     public void onInitializeClient() {
+        KeyBindingHelper.registerKeyBinding(toggleFarmingKey);
     }
 }
